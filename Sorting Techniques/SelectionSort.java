04992c274ee3402e9fcd47e726e8594fef369e14
divyanshu-kumar5572 <divyanshukr0018@gmail.com>
2026-03-27 23:10:46 +0530

    Selection sort algorithm

---
+++ Sorting Techniques/SelectionSort.java
@@ -0,0 +1,23 @@
+public class SelectionSort {
+    public static void printArr(int arr[]){
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+    public static void main(String[] args) {
+        int[] arr = {13,14,11,5,22,34,6};
+        // time complexity = O(n^2)
+        for(int i=0; i<arr.length-1; i++){
+            int smallest = i;
+            for(int j=i+1; j<arr.length; j++){
+                if(arr[smallest]>arr[j]){
+                    smallest = j;
+                }
+            }
+            int temp = arr[smallest];
+            arr[smallest] = arr[i];
+            arr[i] = temp;
+        }
+        printArr(arr);
+    }
+}
